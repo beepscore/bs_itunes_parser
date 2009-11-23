88e40d715555253ec9cd313d6d3143091d313f89
beepscore <support@beepscore.com>
2009-11-22 23:22:51 -0800

    Put Itunes_parser class in ItunesParser module.

--- lib/exercise_parser.rb
+++ lib/exercise_parser.rb
@@ -4,7 +4,7 @@ class Exercise_parser
 
   itunes_xml_file_name = File.dirname(__FILE__) + './test/test_library.xml'
   
-  my_itunes_parser = Itunes_parser.new(itunes_xml_file_name)
+  my_itunes_parser = ItunesParser::Itunes_parser.new(itunes_xml_file_name)
 
   my_itunes_parser.list_summary
   my_itunes_parser.list_first_song
--- lib/itunes_parser.rb
+++ lib/itunes_parser.rb
@@ -1,46 +1,46 @@
 require 'rubygems'
 require 'library'
 
-class Itunes_parser
+module ItunesParser
 
-  def initialize(itunes_xml_file_name)
-    
-    @lib = ItunesParser::Library.new    
-    # @parsed_lib is a hash
-    # @parsed_lib = @lib.parse(File.read(File.dirname(__FILE__) + './test/test_library.xml'))
-    @parsed_lib = @lib.parse(File.read(itunes_xml_file_name))
-    #puts @parsed_lib.inspect
-    
+  class Itunes_parser
+
+    def initialize(itunes_xml_file_name)   
+      @lib = ItunesParser::Library.new    
+      # @parsed_lib is a hash
+      @parsed_lib = @lib.parse(File.read(itunes_xml_file_name))
+      #puts @parsed_lib.inspect   
+    end
+
+    def list_summary  
+      puts "library version #{@parsed_lib['version']}"
+      puts "number of songs #{@parsed_lib['songs'].count}"
+    end
+
+    def list_first_song
+      puts @parsed_lib['first_song'].inspect
+      puts "first song's name #{@parsed_lib['first_song']['name']}"
+      puts "first song's artist #{@parsed_lib['first_song']['artist']}"
+      puts "first song's year #{@parsed_lib['first_song']['year']}"
+      puts "first song's kind #{@parsed_lib['first_song']['kind']}"
+      puts "first song's size #{@parsed_lib['first_song']['size']} bytes"
+
+      # note these tags don't have underscore inserted
+      puts "first song's sample rate #{@parsed_lib['first_song']['sample rate']} Hz"
+      puts "first song's total time #{@parsed_lib['first_song']['total time']} millisec"
+    end
+
+    def populate_metadata 
+      @parsed_lib['songs'].each do |song|
+        puts song
+
+        # song('/key').each do |key|
+        #   song.metadata[key.content.downcase.underscore] = key.next.content
+        # end
+
+      end 
+
+    end
+
   end
-
-  def list_summary  
-    puts "library version #{@parsed_lib['version']}"
-    puts "number of songs #{@parsed_lib['songs'].count}"
-  end
-
-  def list_first_song
-    puts @parsed_lib['first_song'].inspect
-    puts "first song's name #{@parsed_lib['first_song']['name']}"
-    puts "first song's artist #{@parsed_lib['first_song']['artist']}"
-    puts "first song's year #{@parsed_lib['first_song']['year']}"
-    puts "first song's kind #{@parsed_lib['first_song']['kind']}"
-    puts "first song's size #{@parsed_lib['first_song']['size']} bytes"
-
-    # note these tags don't have underscore inserted
-    puts "first song's sample rate #{@parsed_lib['first_song']['sample rate']} Hz"
-    puts "first song's total time #{@parsed_lib['first_song']['total time']} millisec"
-  end
-
-  def populate_metadata 
-    @parsed_lib['songs'].each do |song|
-      puts song
-
-      # song('/key').each do |key|
-      #   song.metadata[key.content.downcase.underscore] = key.next.content
-      # end
-
-    end 
-
-  end
-
 end
