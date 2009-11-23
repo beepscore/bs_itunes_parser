c9e14c0e00db32301cea0468a4d2101fa0b8077f
beepscore <support@beepscore.com>
2009-11-22 22:04:24 -0800

    Refactor rename @result to @parsed_lib.

--- lib/itunes_parser.rb
+++ lib/itunes_parser.rb
@@ -5,24 +5,26 @@ class Itunes_parser
 
   @lib = ItunesParser::Library.new
 
-  # @result is a hash
-  @result = @lib.parse(File.read(File.dirname(__FILE__) + './test/test_library.xml'))
-  puts @result.inspect
+  # @parsed_lib is a hash
+  @parsed_lib = @lib.parse(File.read(File.dirname(__FILE__) + './test/test_library.xml'))
+  puts @parsed_lib.inspect
   
-  puts @result['first_song'].inspect
+  puts @parsed_lib['first_song'].inspect
   
-  puts "library version #{@result['version']}"
+  puts "library version #{@parsed_lib['version']}"
 
-  puts "first song's name #{@result['first_song']['name']}"
-  puts "first song's artist #{@result['first_song']['artist']}"
-  puts "first song's year #{@result['first_song']['year']}"
-  puts "first song's kind #{@result['first_song']['kind']}"
-  puts "first song's size #{@result['first_song']['size']} bytes"
+  puts "first song's name #{@parsed_lib['first_song']['name']}"
+  puts "first song's artist #{@parsed_lib['first_song']['artist']}"
+  puts "first song's year #{@parsed_lib['first_song']['year']}"
+  puts "first song's kind #{@parsed_lib['first_song']['kind']}"
+  puts "first song's size #{@parsed_lib['first_song']['size']} bytes"
   
   # note these tags don't have underscore inserted
-  puts "first song's sample rate #{@result['first_song']['sample rate']} Hz"
-  puts "first song's total time #{@result['first_song']['total time']} millisec"
+  puts "first song's sample rate #{@parsed_lib['first_song']['sample rate']} Hz"
+  puts "first song's total time #{@parsed_lib['first_song']['total time']} millisec"
   
-  puts "number of songs #{@result['songs'].count}"
+  puts "number of songs #{@parsed_lib['songs'].count}"
+  
+  
  
 end
