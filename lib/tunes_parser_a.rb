1f8e9c903813f9846d2c7ff4bc1ada187f7b082e
beepscore <support@beepscore.com>
2009-12-04 22:33:27 -0800

    Use attribute accessor self.parsed_lib instead of parsed_lib

--- lib/tunes_parser_a.rb
+++ lib/tunes_parser_a.rb
@@ -16,42 +16,42 @@ module ItunesParser
     end
 
     def song_count
-      @song_count = parsed_lib['songs'].count
+      @song_count = self.parsed_lib['songs'].count
     end
 
     def list_summary
-      puts "library version #{parsed_lib['version']}"
+      puts "library version #{self.parsed_lib['version']}"
       puts "number of songs #{self.song_count}"
     end    
 
     def list_first_song
-      puts parsed_lib['first_song'].inspect
-      puts "first song's class #{parsed_lib['first_song'].class}"
-      puts "first song's name #{parsed_lib['first_song']['name']}"
-      puts "first song's artist #{parsed_lib['first_song']['artist']}"
-      puts "first song's year #{parsed_lib['first_song']['year']}"
-      puts "first song's kind #{parsed_lib['first_song']['kind']}"
-      puts "first song's size #{parsed_lib['first_song']['size']} bytes"
+      puts self.parsed_lib['first_song'].inspect
+      puts "first song's class #{self.parsed_lib['first_song'].class}"
+      puts "first song's name #{self.parsed_lib['first_song']['name']}"
+      puts "first song's artist #{self.parsed_lib['first_song']['artist']}"
+      puts "first song's year #{self.parsed_lib['first_song']['year']}"
+      puts "first song's kind #{self.parsed_lib['first_song']['kind']}"
+      puts "first song's size #{self.parsed_lib['first_song']['size']} bytes"
       # Note: these tags don't have underscore inserted
-      puts "first song's sample rate #{parsed_lib['first_song']['sample rate']} Hz"
-      puts "first song's total time #{parsed_lib['first_song']['total time']} millisec"
+      puts "first song's sample rate #{self.parsed_lib['first_song']['sample rate']} Hz"
+      puts "first song's total time #{self.parsed_lib['first_song']['total time']} millisec"
     end
 
     def list_songs 
-      parsed_lib['songs'].each do |song|
+      self.parsed_lib['songs'].each do |song|
         puts song.to_s_simple
       end
     end
  
     def find_songs_for_artist
-      songs_for_artist = parsed_lib['songs'].find_all do |song|
+      songs_for_artist = self.parsed_lib['songs'].find_all do |song|
         song.metadata['artist']=='Cause4Concern'
       end
       songs_for_artist
     end
     
     def find_songs_for_key_value(a_key, a_value)
-      songs_for_key_value = parsed_lib['songs'].find_all do |song|
+      songs_for_key_value = self.parsed_lib['songs'].find_all do |song|
         song.metadata[a_key]== a_value
       end
       songs_for_key_value
@@ -59,7 +59,7 @@ module ItunesParser
 
     def count_unique_values_for_key(a_key)
       values_array = [] #array of values
-      parsed_lib['songs'].each do |song|
+      self.parsed_lib['songs'].each do |song|
         values_array << song.metadata[a_key]
       end
       puts values_array.uniq.count
