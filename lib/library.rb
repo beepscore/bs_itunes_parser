52f8c9293301f5138295a4f5f7cb2f9f248f367a
beepscore <support@beepscore.com>
2009-12-13 13:00:55 -0800

    Attempt to parse playlists.  FIXME.

--- lib/library.rb
+++ lib/library.rb
@@ -4,24 +4,28 @@
 require 'active_support'
 require 'nokogiri'
 require 'song'
+require 'playlist'
 
 module ItunesParser
   class Library
 
     attr_accessor :songs
+    attr_accessor :playlists
 
     def initialize
       # instance variable @songs array
       @songs = []
+      @playlists = []
     end
 
     def parse(xml)
       #results hash
       results = {}
-      
+
       doc     = Nokogiri::XML(xml)
-      
+
       version = doc.xpath('/plist/dict/string[1]')[0]
+      results['version'] = version.content
 
       all_songs = doc.xpath('/plist/dict/dict/dict')
 
@@ -29,20 +33,50 @@ module ItunesParser
       results['songs'] = []
 
       all_songs.each do |track|
-        
+
         song = Song.new
-        
+
         track.xpath('./key').each do |key|
           song.metadata[key.content.downcase.underscore] = key.next.content
         end
-        
+
         # The results hash 'songs' key has an array for its value.  Append song to the array.
         results['songs'] << song
       end
 
-      results['version'] = version.content
+      # ===============================
+      #  FIXME: use correct path
+      all_playlists = doc.xpath('/plist/*/key[. = "Playlists"]')
+
+      # In results hash, set key 'playlists' to empty array.  Ref Thomas pg 46        
+      results['playlists'] = []
+
+      all_playlists.each do |track|
+        puts "track child = #{track.child}"
+
+        playlist = Playlist.new
+
+        track.xpath('./key').each do |key|
+          playlist.metadata[key.content.downcase.underscore] = key.next.content
+        end
+
+        # The results hash 'playlists' key has an array for its value.  Append playlist to the array.
+        results['playlists'] << playlist
+      end
+      
+      # ===============================
+      # FIXME
+      # find all tags with a key of Playlists
+      # Search for nodes by xpath
+      puts doc.search('/*/*/key[. = "Playlists"]')
+
+      # doc.xpath('/*/*/key[. = "Playlists"]').each do |a_tag|
+      #   puts a_tag.content
+      # end  
+      # ===============================
 
       results
-    end
-  end
-end
+    end #parse
+
+  end #Library
+end #ITunesParser
